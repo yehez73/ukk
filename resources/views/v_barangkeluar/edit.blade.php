55ba6e7ad069ac979cb646343f0cabe8dcac7ac7
yehez73 <wearyspider159357@gmail.com>
2024-06-21 13:23:24 +0700

    yes

--- resources/views/v_barangkeluar/edit.blade.php
+++ resources/views/v_barangkeluar/edit.blade.php
@@ -7,19 +7,15 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        @if(session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-                        <form action="{{ route('barangkeluar.store') }}" method="POST" enctype="multipart/form-data">                    
+                        <form action="{{ route('barangkeluar.update',$rsetBarangKeluar->id) }}" method="POST" enctype="multipart/form-data">                    
                             @csrf
+                            @method('PUT')
 
                             <div class="form-group">
                                 <label class="font-weight-bold">Tanggal Keluar</label>
-                                <input type="date" class="form-control @error('tgl_keluar') is-invalid @enderror" name="tgl_keluar" value="{{ old('tgl_keluar') }}" placeholder="Masukkan tgl_keluar">
+                                <input type="date" class="form-control @error('tgl_keluar') is-invalid @enderror" name="tgl_keluar" value="{{ old('tgl_keluar',$rsetBarangKeluar->tgl_keluar) }}" placeholder="Masukkan Nama Barang">
                             
-                                <!-- error message untuk tgl_keluar -->
+                                <!-- error message untuk nama -->
                                 @error('tgl_keluar')
                                     <div class="alert alert-danger mt-2">
                                         {{ $message }}
@@ -29,7 +25,7 @@
 
                             <div class="form-group">
                                 <label class="font-weight-bold">Quantity</label>
-                                <input type="number" class="form-control @error('qty_keluar') is-invalid @enderror" name="qty_keluar" value="{{ old('qty_keluar') }}" placeholder="Masukkan Quantity">
+                                <input type="text" class="form-control @error('qty_keluar') is-invalid @enderror" name="qty_keluar" value="{{ old('qty_keluar',$rsetBarangKeluar->qty_keluar) }}" placeholder="Masukkan Quantity">
                             
                                 <!-- error message untuk qty_keluar -->
                                 @error('qty_keluar')
@@ -42,14 +38,12 @@
                             <div class="form-group">
                                 <label class="font-weight-bold">Barang</label>
                                 <select class="form-control @error('barang_id') is-invalid @enderror" name="barang_id">
-                                    @foreach($barangID as $barangIdrow)
-                                        <option value="{{ $barangIdrow->id }}" {{ old('barang_id') == $barangIdrow->id ? 'selected' : '' }}>
-                                            {{ $barangIdrow->merk }} {{ $barangIdrow->seri }} || Stok: {{ $barangIdrow->stok }}
-                                        </option>
+                                    <option value="">Pilih Barang</option>
+                                    @foreach($barangID as $barang)
+                                        <option value="{{ $barang->id }}" {{ old('barang_id', $rsetBarangKeluar->barang_id) == $barang->id ? 'selected' : '' }}>{{ $barang->merk }} - Stok : {{ $barang->stok }}</option>
                                     @endforeach
                                 </select>
-                            
-                                <!-- error message untuk barang_id -->
+
                                 @error('barang_id')
                                     <div class="alert alert-danger mt-2">
                                         {{ $message }}
@@ -57,12 +51,18 @@
                                 @enderror
                             </div>
 
+
+                            
+
                             <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
                             <button type="reset" class="btn btn-md btn-warning">RESET</button>
 
                         </form> 
                     </div>
                 </div>
+
+ 
+
             </div>
         </div>
     </div>
